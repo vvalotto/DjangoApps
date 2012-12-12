2f1b25758f35f2ad20eaa0d366002d43d8d6ead5
Victor Valotto <vvalotto@gmail.com>
2012-12-12 12:44:03 -0300

    Nuevo commit

--- MisMaterias/MisMaterias/settings.py
+++ MisMaterias/MisMaterias/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('Victor Valotto', 'vvalotto@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -116,9 +116,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
